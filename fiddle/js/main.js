e03bad28c7f5901fa30daa6b22555a402fbc0094
Justineo <justice360@gmail.com>
2014-12-09 01:03:42 +0800

    fix less path

--- fiddle/js/main.js
+++ fiddle/js/main.js
@@ -144,7 +144,9 @@
             less = _LESS_VERSIONS[version];
             updateParser();
         } else {
-            loadScript('https://rawgit.com/less/less.js/v' + version + '/dist/less.min.js', function () {
+            var url = 'https://rawgit.com/less/less.js/v' + version
+                + '/dist/less' + (version.charAt(0) === 1 ? '-' + version : '') + '.min.js';
+            loadScript(url, function () {
                 _LESS_VERSIONS[version] = less;
                 updateParser();
             });
