995401e40d60238ba89ca26250365afd7f3731da
Justineo <justice360@gmail.com>
2015-03-30 14:10:09 +0800

    add demo compilation

--- compile-demo.js
+++ compile-demo.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var less = require('less');
+var ProgressBar = require('progress');
 var Est = require('./lib/index');
 
 var index = fs.readFileSync('index-source.html', {
     encoding: 'utf8'
 });
 
+console.log('Preparing...');
 var pattern = /(\s+)(<pre class="usage"><code class="less">)([\s\S]+?)(<\/code><\/pre>)/g;
 
 var codes = [];
@@ -16,6 +18,7 @@ index.replace(pattern, function (match, indent, start, code, end) {
 
 var out = {};
 var remaining = codes.length;
+var bar = new ProgressBar('Compiling... :percent :elapseds', { total: remaining });
 for (var i = 0, j = remaining; i < j; i++) {
     var code = codes[i];
     (function (code) {
@@ -39,15 +42,17 @@ for (var i = 0, j = remaining; i < j; i++) {
 
 function done() {
     remaining--;
-    console.log(remaining);
+    bar.tick();
 
     if (remaining === 0) {
         var newIndex = index.replace(pattern, function (match, indent, start, code, end) {
             return indent + start + code + end + '\n' + indent + '<pre class="usage"><code class="css">' + out[code] + end;
         });
 
-        fs.writeFileSync(__dirname + '/index2.html', newIndex, {
+        fs.writeFileSync(__dirname + '/index.html', newIndex, {
             encoding: 'utf8'
         });
+
+        console.log('All done!');
     }
 }
